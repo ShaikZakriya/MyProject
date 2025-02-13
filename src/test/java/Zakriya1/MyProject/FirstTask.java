5a5d26075cd3e49c91fae5e48e4f172463eceb70
ShaikZakriya <155053622+ShaikZakriya@users.noreply.github.com>
2025-02-13 14:00:02 +0530

    Sum
    Updated

--- src/test/java/Zakriya1/MyProject/FirstTask.java
+++ src/test/java/Zakriya1/MyProject/FirstTask.java
@@ -4,8 +4,12 @@ public class FirstTask {
 
 	public static void main(String[] args) {
 		
-		System.out.println();	
 		
+		
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println("Sum: " + sum);
 	}
 
 }
