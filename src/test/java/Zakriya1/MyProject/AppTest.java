1a0a5c36ca4a0bb1adfa0682f4f3aa900effac5f
ShaikZakriya <155053622+ShaikZakriya@users.noreply.github.com>
2025-02-13 07:54:07 +0530

    Initial commit

---
+++ src/test/java/Zakriya1/MyProject/AppTest.java
@@ -0,0 +1,19 @@
+package Zakriya1.MyProject;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
